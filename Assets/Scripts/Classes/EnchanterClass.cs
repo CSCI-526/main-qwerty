357886595a941adfdc6450ab489303afca502354
tsaiyu-chejay <jay89216@gmail.com>
2025-11-13 15:44:26 -0800

    enchanter update

--- Assets/Scripts/Classes/EnchanterClass.cs
+++ Assets/Scripts/Classes/EnchanterClass.cs
@@ -3,33 +3,40 @@ using UnityEngine;
 
 public class EnchanterClass : ClassBase
 {
+    private float modMultipler = 1.2f;
     public override void Ability1(ulong playerID, TargetableController target, int baseValue)
     {
-        /*if (targetID != self){
-            gameManager.addBuffDebuffToListRpc(targetType, self, 0.6f, 1, "DamageBuff");
-            gameManager.playerDealDamageRpc(self, targetType, self, baseValue);
-         }*/
-        //gameManager.addBuffDebuffToListRpc(targetType, targetingID, 0.6f, 1, "DamageBuff");
-        //gameManager.playerDealDamageRpc(playerID, targetType, targetingID, baseValue);
+        ulong targetType = DetermineTargetType(target);
+        gameManager.addBuffDebuffToListRpc(0, playerID, 0.6f, 1, "DamageBuff");
+        int mod = gameManager.typingEffectManager.ApplyEffectOnMod()[2];
+        int delta = Math.Min((int)(-baseValue / Math.Pow(modMultipler, mod)), -1);
+        gameManager.playerDealDamageRpc(playerID, targetType, target.targetingID.Value, delta);
         LogAbility("EnchanterClass", 1, "Attack (0.6x base value)");
     }
 
     public override void Ability2(ulong playerID, TargetableController target, int baseValue)
     {
-        /*if (targetID != self){
-            gameManager.addBuffDebuffToListRpc(targetType, targetingID, 0.3f, 1, "LeechBuff");
-         }*/
-        //gameManager.addBuffDebuffToListRpc(targetType, targetingID, 0.3f, 1, "LeechBuff");
+        ulong targetType = DetermineTargetType(target);
+        if (target.targetingID.Value != playerID){
+            gameManager.addBuffDebuffToListRpc(0, playerID, 0.3f, 1, "LeechBuff");
+         }
+        gameManager.addBuffDebuffToListRpc(targetType, target.targetingID.Value, 0.3f, 1, "LeechBuff");
         LogAbility("EnchanterClass", 2, "Next attack leeches 30% of damage done");
     }
 
     public override void Ability3(ulong playerID, TargetableController target, int baseValue)
     {
-        /*if (targetID != self){
-            //shield
-         }*/
-        //shield
-        LogAbility("EnchanterClass", 3, "Grant target a damage shield that blocks X damage");
+        ulong targetType = DetermineTargetType(target);
+        int mod = gameManager.typingEffectManager.ApplyEffectOnMod()[1];
+        int delta = Math.Max((int)(baseValue / Math.Pow(modMultipler, mod)), 1);
+        if (target.targetingID.Value != playerID)
+        {
+            gameManager.playerHealRpc(playerID, 0, playerID, delta);
+            //target.maxHealth.Value += delta;
+        }
+        gameManager.playerHealRpc(playerID, targetType, target.targetingID.Value, delta);
+        //target.maxHealth.Value += delta;
+        LogAbility("EnchanterClass", 3, "Grant target a damage shield of x% base value");
     }
 
     public override void Ability4(ulong playerID, TargetableController target, int baseValue)
